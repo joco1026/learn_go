c7df5fe30caaf1482a7684d13f3170fab53b8149
Josh Cohen <jcohen@medivo.com>
2015-05-07 13:38:39 -0400

    Problem 1, added stdin read, outputs md5sum

--- problem1/md5checksum.go
+++ problem1/md5checksum.go
@@ -1,54 +1,38 @@
 package main
 
 import (
-  "io"
-)
-
-func main() {
-  file := "checkme"
-  contents := io.ReadFile(file)
-
-}
-
-
-
-/*
-
-
-
-import (
-    "crypto/md5"
-    "fmt"
-    "io"
-)
-
-func main() {
-    h := md5.New()
-    io.WriteString(h, "The fog is getting thicker!")
-    fmt.Printf("%x", h.Sum(nil))
-}
-
---------
-
-import (
-  "bufio"
+  "io/ioutil"
   "crypto/md5"
   "fmt"
   "os"
 )
 
 func main() {
-  data := bufio.NewReader(os.Stdin)
-  stats, _ := os.Stdin.Stat()
 
-  if stats.Size() > 0 {
-    str, _, _ := data.ReadLine()
-    fmt.Printf("%x\n", md5.Sum([]byte(str)))
-  } else {
-    fmt.Printf("%x\n", md5.Sum([]byte("")))
+  contents, err := ioutil.ReadFile("checkme")
+  input, err := ioutil.ReadAll(os.Stdin)
+
+  if err != nil {
+    panic(err)
+  }
+
+  inputhasher := md5.New()
+  inputhasher.Write(input)
+
+  filehasher := md5.New()
+  filehasher.Write(contents)
+
+  //print contents of file and its hash
+  fmt.Printf("%s\n", contents)
+  fmt.Printf("%x\n", filehasher.Sum(nil))
+
+  //print standard input and its hash
+  fmt.Printf("%s\n", input)
+  fmt.Printf("%x\n", inputhasher.Sum(nil))
+
+  if err != nil {
+    panic(err)
   }
 }
 
---------
-*/
 
